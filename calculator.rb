afcdd8fc9e0ca692344f422fcfee139d87875772
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-18 23:45:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,5 @@ end
 def calculater 
   first_number = get_variable_from_file('./calculator.rb', "first_number")
   second_number = get_variable_from_file('./calculator.rb', "second_number")
-  
+  sum = get_variable_from_file('./calculator.rb', "sum")
 end
