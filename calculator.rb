f5eb6fff4e38c8cb779e877f35d14af539bf596c
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-18 23:38:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@ def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
 def calculater 
-  first_number = #{variable)
+  first_number = get_variable_from_file('./calculator.rb', "first_number")
  end
