9982ae4a2c137e6dc466fe220719f564b3963065
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 00:10:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-
+do
 
 first_number=1
 second_number=2
@@ -7,10 +7,4 @@ difference  = first_number - second_number
 product = first_number*second_number
 quotient = first_number/second_number
 
-def calculater 
-  first_number = get_variable_from_file('./calculator.rb', "first_number")
-  second_number = get_variable_from_file('./calculator.rb', "second_number")
-  sum = get_variable_from_file('./calculator.rb', "sum")
-  product = get_variable_from_file('./calculator.rb', "product")
-  quotient = get_variable_from_file('./calculator.rb', "quotient")
 end
