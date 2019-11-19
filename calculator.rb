570ff2aa47e2616812b77355befc59bf8d1ee58c
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 00:09:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,12 +3,14 @@ def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
 end
+do
 first_number=1
 second_number=2
 sum = first_number + second_number
 difference  = first_number - second_number
 product = first_number*second_number
 quotient = first_number/second_number
+end
 def calculater 
   first_number = get_variable_from_file('./calculator.rb', "first_number")
   second_number = get_variable_from_file('./calculator.rb', "second_number")
