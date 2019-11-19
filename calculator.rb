0e8df0b42f599a376c3dbe6583151b010d602f2b
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 00:02:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,6 +6,7 @@ end
 first_number=1
 second_number=2
 sum = first_number + second_number
+difference  = first_number - second_number
 product = first_number*second_number
 quotient = first_number/second_number
 def calculater 
