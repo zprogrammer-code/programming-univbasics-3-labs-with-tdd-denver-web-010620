86cfdd5fc07e563f981343e0866eaf574f2f5216
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 00:11:27 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+
+
+first_number=1
+second_number=2
+sum = first_number + second_number
+difference  = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
+
